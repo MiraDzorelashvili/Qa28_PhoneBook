3cfb6f5806d0ef6ff7dce67da5c2ff7f49194c87
MiraDzorelashvili <miradzorelashvili15@gmail.com>
2025-02-08 14:44:08 +0200

    added Classwork

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,38 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTests extends TestBase{
+public class LoginTests extends TestBase {
+
+    @BeforeMethod
+    public void preCondition(){
+        //if SignOut present --->logout
+        if(app.getHelperUser().isLogged())
+            app.getHelperUser().logout();
+    }
+
 
     @Test
-    public void test(){
-
+    public void loginSuccess() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("mira@gmail.com","Mmira1234$");
+        app.getHelperUser().sumitLogin();
+        //Assert
+//        Assert.assertEquals();
+//        Assert.assertNotEquals();
+//        Assert.assertTrue()
+//        Assert.assertFalse();
+        Assert.assertTrue(app.getHelperUser().isLogged());
     }
+
+    @Test
+    public void loginSuccessModel() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("mira@gmail.com","Mmira1234$");
+        app.getHelperUser().sumitLogin();
+        Assert.assertTrue(app.getHelperUser().isLogged());
+    }
+
 }
